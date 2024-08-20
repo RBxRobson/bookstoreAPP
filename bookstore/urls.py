52874ec77196aa088092e6c9e5dd266d0625c2ea
RBxRobson <robsonnbarbosa0@gmail.com>
2024-08-20 17:36:49 -0300

    feat: Adiciona novas rotas para os viewsets order e product

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
+    # Rota para a aplicação 'order'
+    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+
+    # Rota para a aplicação 'product'
+    re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
 ]
