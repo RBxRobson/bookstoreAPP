7135fa2cd2351d6ecc6cef26c04f5dba8b13f226
RBxRobson <robsonnbarbosa0@gmail.com>
2024-08-22 18:06:05 -0300

    feat: Adiciona viewset de category

---
+++ product/viewsets/category_viewset.py
@@ -0,0 +1,15 @@
+from rest_framework.viewsets import ModelViewSet
+
+from product.models import Category
+from product.serializers.category_serializer import CategorySerializer
+
+
+# ViewSet para o modelo Category.
+class CategoryViewSet(ModelViewSet):
+    # Especifica o serializer que será usado para as operações neste ViewSet.
+    serializer_class = CategorySerializer
+
+    # Define o queryset que será usado para buscar os dados. 
+    # Neste caso, todas as categorias serão retornadas, ordenadas pelo campo "id".
+    def get_queryset(self):
+        return Category.objects.all().order_by("id")
