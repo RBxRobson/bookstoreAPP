b751e606828e7ff8f41067db9eecf6dddd5146c4
RBxRobson <robsonnbarbosa0@gmail.com>
2024-10-25 10:20:54 -0300

    fix: Corrige endereço errado

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 SECRET_KEY = "django-insecure-f*k@=53bc5!shef1-6w+m$-g)kspbaljz%8k4(j7iuc-u2_dyd"
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://rbxrobson.pythonanywhere.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rbxrobson.pythonanywhere.com']
 
 # Application definition
 INSTALLED_APPS = [
